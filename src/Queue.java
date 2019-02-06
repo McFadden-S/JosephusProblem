ebb7ab5be64b5c8b7e6e795fac59bf617a2cb03e
McFadden-S <shaefromhome@shaw.ca>
2019-02-06 17:38:23 -0600

    Initial commit

---
+++ src/Queue.java
@@ -0,0 +1,112 @@
+
+import java.util.ArrayList;
+
+/* **********************************************************
+ * Programmer:	Shae McFadden
+ * Class:	CS40S
+ * 
+ * Assignment:	3.1
+ *
+ * Description:	This class will implement a int queue data structure
+ *
+ * 
+ * *************************************************************
+ */
+ 
+ // import files here as needed
+ 
+ 
+ public class Queue
+ {  // begin class
+ 	
+ 	// *********** class constants **********
+ 	// ********** instance variable **********
+ 	
+        int front = 0; //front of queue
+        int sizeQ = 0; //current size of queue
+        ArrayList<Integer> queue = new ArrayList<>(); //holds queue values
+     
+ 	// ********** constructors ***********
+ 	
+ 	/*
+         Purpose: default constructor for Queue object
+         Interface: none
+         Return: none
+        */
+        public Queue(){
+            
+        }//end of default constructer
+        
+        /*
+         Purpose: initalized constructor for Queue object
+         Interface: size (int), values (int[])
+         Return: none
+        */
+        public Queue(int[] n){
+            for(int i = 0; i < n.length; i++){
+                queue.add(n[i]);
+                sizeQ++;
+            }//end of for loop
+        }//end of initalized constructor
+     
+ 	// ********** accessors **********
+ 	
+        /*
+        Purpose: checks if Queue is empty
+        Interface: none
+        Returns: boolean true if empty
+        */
+        public boolean Empty(){
+            boolean em = true;
+            
+            if(sizeQ>0){
+                em = false;
+            }
+            
+            return em;
+        }//end of empty
+        
+        /*
+        Purpose: reads front value of queue
+        Interface: none
+        Returns: int of top value of Queue, zero if none
+        */
+        public int Peek(){
+            int n = 0;
+            
+            if (!(this.Empty())){
+                n = queue.get(front);
+            }//end of if
+            
+            return n;
+        }//end of peek
+        
+ 	// ********** mutators **********
+        
+        /*
+        Purpose: adds value to back of queue
+        interface: value to be added (int)
+        returns: none
+        */
+        public void Enque(int n){
+            queue.add(n);
+            sizeQ++;
+        }//end of enque
+        
+        /*
+        Purpose: removes value of the queue
+        interface none
+        returns value removed
+        */
+        public int Deque(){
+            int n = 0;
+            
+            if(!(this.Empty())){
+                n = queue.get(front);
+                queue.remove(front);
+                sizeQ--;
+            }//end of if    
+            return n;
+        }//end of Deque
+ 
+ }  // end class
---
+++ src/main.java
@@ -0,0 +1,50 @@
+// **********************************************************************
+// Programmer:	Shae McFadden
+// Class:	CS40S
+//
+// Assignment:	3.2
+//
+// Description:	This program will calculate the winning seat in a modified
+//              Josephus Problem
+//
+// Input:	Will read in number of people and how many over is killed
+//              from file
+//  Output:	will print order of elimination
+// ***********************************************************************
+
+import javax.swing.*;
+import java.text.DecimalFormat;
+
+public class main
+{  // begin class
+	public static void main(String args[])
+	{  // begin main
+	// ***** declaration of constants *****
+	
+	// ***** declaration of variables *****
+	
+	// ***** create objects *****
+		
+		
+	// ***** create input stream *****
+	
+		
+		
+	// ***** Print Banner *****
+	
+		System.out.println("**********************************");
+		System.out.println("NAME:        Shae McFadden");
+		System.out.println("Class:       CS40S");
+		System.out.println("Assignment:  3.2");
+		System.out.println("**********************************");
+		
+	// ***** main *****
+	
+            
+
+	// ***** closing message *****
+	
+		System.out.println("end of processing");
+	
+	}  // end main	
+}  // end class
