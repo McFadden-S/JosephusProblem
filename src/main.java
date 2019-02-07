4a6b3ed537fea6fe36ea2449de36f8f726f9e46b
McFadden-S <shaefromhome@shaw.ca>
2019-02-06 18:12:44 -0600

    V1
    Working Josephus Problem Solver

--- src/main.java
+++ src/main.java
@@ -12,6 +12,10 @@
 //  Output:	will print order of elimination
 // ***********************************************************************
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import javax.swing.*;
 import java.text.DecimalFormat;
 
@@ -23,12 +27,27 @@ public class main
 	
 	// ***** declaration of variables *****
 	
+            String delim = "[ ]+";
+            String strin;
+            String[] tokens;
+            
+            int counter = 0;
+            int n = 0;
+            int people = 0;
+            int temp = 0;
+            
+            String elimOut = "";
+            int winner = 0;
 	// ***** create objects *****
 		
-		
+                Queue queue = new Queue();
+		BufferedReader fin;
+                
 	// ***** create input stream *****
 	
-		
+        try{
+
+            fin = new BufferedReader(new FileReader("JosephusData.txt"));
 		
 	// ***** Print Banner *****
 	
@@ -40,8 +59,49 @@ public class main
 		
 	// ***** main *****
 	
+            strin = fin.readLine();
             
+            while(strin!=null){
+                tokens = strin.split(delim);
 
+                n = Integer.parseInt(tokens[0]);
+                people = Integer.parseInt(tokens[1]);
+
+                for(int i = 0; i<people; i++){
+                    queue.Enque(i+1);
+                }//end of for loop
+
+
+                while(queue.sizeQ != 1){
+                    temp = queue.Deque();
+                    if (counter==n-1){
+                        counter=0;
+                        elimOut += (temp + " ");
+                    }//end of if to be killed
+                    else{
+                        queue.Enque(temp);
+                        counter++;
+                    }//end of else
+                }//end of Josephus' game
+                
+                winner = queue.Deque();
+                
+                System.out.println("Josephus: Eliminate every " + n + " # of People " + people);
+                System.out.println("Order of Elimination " + elimOut);
+                System.out.println("Winning Seat: " + winner);
+                System.out.println("**************************************");
+                
+                counter = 0;
+                elimOut = "";
+                strin = fin.readLine();
+            }//end of while loop
+        }//end of try
+        catch(FileNotFoundException e){
+            System.out.println("File Not Found");
+        }//end of catch
+        catch(IOException e){
+
+        }//end of catch
 	// ***** closing message *****
 	
 		System.out.println("end of processing");
